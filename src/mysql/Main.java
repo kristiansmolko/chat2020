68ee2a9f838805230aea7e1c209b6da2955e332b
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-11 13:23:26 +0100

    Modified main to print my messages

--- src/mysql/Main.java
+++ src/mysql/Main.java
@@ -1,13 +1,19 @@
 package mysql;
 
+import mysql.entity.Message;
 import mysql.helper.Help;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Database dat = new Database();
         String name = "kristianS";
         String pass = TopSecretData.getChatPass();
-        dat.changePassword(name, "kristian", pass, pass);
+        List<Message> list = dat.getMyMessages(name);
+        for (Message m : list){
+            System.out.println(m.getFrom() + " to " + m.getTo() + " at " + m.getDt() + " : " + m.getText());
+        }
     }
 
 }
