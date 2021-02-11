e90742cae9598d90fd513b08a6d5057b704157f8
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-11 15:34:03 +0100

    Added graphical interface

--- src/mysql/Main.java
+++ src/mysql/Main.java
@@ -1,19 +1,56 @@
 package mysql;
 
-import mysql.entity.Message;
-import mysql.helper.Help;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-import java.util.List;
+import static mysql.graphics.Graphics.*;
 
-public class Main {
+public class Main extends Application {
+    Database dat = new Database();
+    String name = "kristianS";
+    String pass = TopSecretData.getChatPass();
+
     public static void main(String[] args) {
-        Database dat = new Database();
-        String name = "kristianS";
-        String pass = TopSecretData.getChatPass();
-        List<Message> list = dat.getMyMessages(name);
+        launch(args);
+
+        /*List<Message> list = dat.getMyMessages(name);
         for (Message m : list){
             System.out.println(m.getFrom() + " to " + m.getTo() + " at " + m.getDt() + " : " + m.getText());
-        }
+        }*/
+
+
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = loginScreen();
+        Scene scene = new Scene(root, 400,400);
+        loginButton.setOnMouseClicked(e -> {
+            dat.login(loginInput.getText(), loginPassword.getText());
+
+        });
+        registerButton.setOnMouseClicked(e -> {
+            dat.register(registerInput.getText(), pf.getText(), pf2.getText());
+            registerInput.setText("");
+            pf.setText("");
+            pf2.setText("");
+        });
+        signUp.setOnMouseClicked(e -> {
+            BorderPane register = registerScreen();
+            Scene registerScene = new Scene(register, 400, 400);
+            stage.setScene(registerScene);
+            stage.setTitle("Register");
+            stage.show();
+        });
+        signIn.setOnMouseClicked(e -> {
+            stage.setScene(scene);
+            stage.setTitle("Login");
+            stage.show();
+        });
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
