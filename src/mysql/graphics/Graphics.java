d6f422f95f7b0cf339a65f6685450c01cf027474
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-15 13:34:14 +0100

    Added dialogs and more

--- src/mysql/graphics/Graphics.java
+++ src/mysql/graphics/Graphics.java
@@ -1,8 +1,11 @@
 package mysql.graphics;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,10 +16,14 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 import mysql.Database;
 import mysql.entity.Message;
 import mysql.entity.User;
@@ -97,7 +104,9 @@ public class Graphics {
     }
 
     public static BorderPane messagesScreen(User user){
+        Database dat = new Database();
         BorderPane root = new BorderPane();
+        root.setStyle("-fx-padding: 20");
         List<Message> listOfAll = Help.getAllMessages();
         List<Message> listOfMy = Help.getMyMessages(user.getLogin());
         List<String> listOfUsers = Help.getUsers();
@@ -126,12 +135,21 @@ public class Graphics {
         buttons.setHgap(20);
         buttons.addRow(0, myMessages, allMessages);
 
+        BorderPane rightSide = new BorderPane();
+
         GridPane userPane = new GridPane();
+        userPane.setStyle("-fx-padding: 0 10");
         userPane.setVgap(20);
         for (int i = 0; i < listOfUsers.size(); i++){
             Label userLabel = new Label(listOfUsers.get(i));
+            userLabel.setOnMouseClicked(e -> {
+                dialogMessage(userLabel.getText(), user);
+            });
             userPane.addRow(i, userLabel);
         }
+
+        rightSide.setTop(getLog(user));
+        rightSide.setCenter(userPane);
 
         BorderPane messagesPane = new BorderPane();
         messagesPane.setMaxWidth(width - 200);
@@ -139,7 +157,7 @@ public class Graphics {
         messagesPane.setCenter(messagesArea);
 
         root.setCenter(messagesPane);
-        root.setRight(userPane);
+        root.setRight(rightSide);
         return root;
     }
 
@@ -217,6 +235,167 @@ public class Graphics {
         };
     }
 
+    private static void dialogMessage(String name, User user){
+        Database dat = new Database();
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
 
+        BorderPane dialog = new BorderPane();
+        Label nameLabel = new Label("Message to: " + name);
+        nameLabel.setFont(Font.font(15));
+        nameLabel.setTranslateX(100);
+
+        Label messageSent = new Label("Message sent!");
+        messageSent.setVisible(false);
+        messageSent.setTextFill(Color.GREEN);
+        messageSent.setFont(Font.font(20));
+        //message will display for few seconds
+        Timeline timer = new Timeline(
+                new KeyFrame(Duration.millis(1), e2 -> messageSent.setVisible(true)),
+                new KeyFrame(Duration.millis(3000), e2 -> messageSent.setVisible(false))
+        );
+        timer.setCycleCount(1);
+
+        Label wrongMessage = new Label("Error sending message!");
+        wrongMessage.setVisible(false);
+        wrongMessage.setTextFill(Color.RED);
+        wrongMessage.setFont(Font.font(20));
+        Timeline wrongTimer = new Timeline(
+                new KeyFrame(Duration.millis(1), e2 -> wrongMessage.setVisible(true)),
+                new KeyFrame(Duration.millis(3000), e2 -> wrongMessage.setVisible(false))
+        );
+        wrongTimer.setCycleCount(1);
+
+        timer.setOnFinished(e -> {
+            stage.close();
+        });
+
+        TextArea messageText = new TextArea();
+        messageText.setMaxSize(250,250);
+        Button sendMessage = new Button("Send");
+        sendMessage.setOnAction(e -> {
+            boolean sent = dat.sendMessage(user.getId(), name, messageText.getText());
+            if (sent)
+                timer.play();
+            else {
+                wrongTimer.play();
+                messageText.setText("");
+            }
+        });
+
+        dialog.setTop(nameLabel);
+        StackPane messageArea = new StackPane();
+        messageArea.getChildren().addAll(messageText, messageSent, wrongMessage);
+        dialog.setCenter(messageArea);
+        dialog.setBottom(sendMessage);
+
+        Scene scene = new Scene(dialog, 300, 300);
+        stage.setScene(scene);
+        stage.setTitle("Send message");
+        stage.showAndWait();
+    }
+
+    private static void changePassDialog(User user){
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        Database dat = new Database();
+        BorderPane root = new BorderPane();
+        StackPane passwordsPane = new StackPane();
+        GridPane passwords = new GridPane();
+        passwords.setStyle("-fx-padding: 10");
+        passwords.setVgap(10);
+        passwords.setHgap(10);
+        TextField oldPassword = new TextField();
+        TextField newPassword = new TextField();
+        TextField newPassword2 = new TextField();
+        passwords.add(new Label("Old password:"), 0, 0);
+        passwords.add(new Label("New password:"), 0, 1);
+        passwords.add(new Label("New password:"), 0, 2);
+        passwords.add(oldPassword, 1, 0);
+        passwords.add(newPassword, 1, 1);
+        passwords.add(newPassword2, 1, 2);
+
+        Label changedPass = new Label("Password changed!");
+        changedPass.setVisible(false);
+        changedPass.setTextFill(Color.GREEN);
+        changedPass.setFont(Font.font(20));
+        Timeline timer = new Timeline(
+                new KeyFrame(Duration.millis(1), e2 -> changedPass.setVisible(true)),
+                new KeyFrame(Duration.millis(3000), e2 -> changedPass.setVisible(false))
+        );
+        timer.setCycleCount(1);
+        timer.setOnFinished(e -> stage.close());
+        Label wrongPass = new Label("Error changing password");
+        wrongPass.setVisible(false);
+        wrongPass.setTextFill(Color.RED);
+        wrongPass.setFont(Font.font(20));
+        Timeline wrongTimer = new Timeline(
+                new KeyFrame(Duration.millis(1), e2 -> wrongPass.setVisible(true)),
+                new KeyFrame(Duration.millis(3000), e2 -> wrongPass.setVisible(false))
+        );
+        wrongTimer.setCycleCount(1);
+
+        Button change = new Button("Change");
+        change.setTranslateX(50);
+        change.setOnAction(e -> {
+            if (!newPassword.equals(newPassword2) || !oldPassword.equals(user.getPassword())){
+                wrongTimer.play();
+            } else {
+                dat.changePassword(user.getLogin(),
+                        oldPassword.getText(),
+                        newPassword.getText(),
+                        newPassword2.getText());
+                timer.play();
+            }
+        });
+
+        passwordsPane.getChildren().addAll(passwords, changedPass, wrongPass);
+
+        Label label = new Label("Change password");
+        label.setTranslateX(70);
+        root.setTop(label);
+        root.setCenter(passwordsPane);
+        root.setBottom(change);
+
+        Scene scene = new Scene(root, 300, 180);
+        stage.setScene(scene);
+        stage.setTitle("Change password");
+        stage.showAndWait();
+    }
+
+    private static BorderPane getLog(User user){
+        BorderPane log = new BorderPane();
+        log.setStyle("-fx-padding: 10");
+
+        StackPane userInt = new StackPane();
+        Circle circle = new Circle(30, Color.AZURE);
+        Label userName = new Label(String.valueOf(user.getLogin().charAt(0)));
+        userName.setFont(Font.font(20));
+        userInt.getChildren().addAll(circle, userName);
+
+        GridPane options = new GridPane();
+        options.setStyle("-fx-padding: 20; " +
+                "-fx-border-style: solid;" +
+                "-fx-border-width: 1;" +
+                "-fx-border-color: gray");
+        options.setVgap(10);
+        options.setVisible(false);
+
+        userInt.setOnMouseClicked(e -> options.setVisible(true));
+        Button changePass = new Button("Change password");
+        changePass.setOnAction(e -> {
+            options.setVisible(false);
+            changePassDialog(user);
+        });
+        Button logOut = new Button("Log out");
+        logOut.setOnAction(e -> System.exit(0));
+        options.addRow(0, new Label("Logged as: " + user.getLogin()));
+        options.addRow(1, changePass);
+        options.addRow(2, logOut);
+
+        log.setTop(userInt);
+        log.setCenter(options);
+        return log;
+    }
 
 }
